c9851b4b237624088ea9d1f84bed97fbccb86d2e
dev1 <dev@zheteng.cloud>
2021-08-24 08:16:04 +0800

    oss body concat

--- src/aliyun.rs
+++ src/aliyun.rs
@@ -7,6 +7,35 @@ fn sign_base64(key_secret: &str, body: &str) -> String {
     base64::encode(mac.result().code())
 }
 
+pub mod oss {
+    pub struct Body {
+        pub verb: String,
+        pub content_md5: String,
+        pub content_type: String,
+        pub date: String,
+        pub canonicalized_ossheaders: String,
+        pub canonicalized_resource: String,
+    }
+
+    impl ToString for Body {
+        fn to_string(&self) -> String {
+            format!(
+                "{}\n{}\n{}\n{}\n{}{}",
+                self.verb,
+                self.content_md5,
+                self.content_type,
+                self.date,
+                self.canonicalized_ossheaders,
+                self.canonicalized_resource
+            )
+        }
+    }
+
+    fn concat_auth(key_id: &str, signature: &str) -> String {
+        format!("OSS {}:{}", key_id, signature)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,6 +44,6 @@ mod tests {
     fn base64() {
         let key1 = "key1";
         let body1 = "body1";
-        assert_eq!(sign_base64(key1,body1), "u3fznj0yiE48+1xlkideoCqhhdc=")
+        assert_eq!(sign_base64(key1, body1), "u3fznj0yiE48+1xlkideoCqhhdc=")
     }
 }
